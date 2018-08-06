f220fbe80463ab8065acef91525ddd6e2a53feb5
adieliz <40433611+adieliz@users.noreply.github.com>
2018-08-05 22:56:34 -0400

    psuedo code homework
    psuedo code for recipe project

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//when user clicks on hamburger (in mobile and smaller screen state), nav slides down
+
+//when user clicks again on hamburger, expanded nav slides up
+
+//when user clicks on "more deliciousness," below each of the recipe sections, links to more recipes appear. (to happen below each of the six sections)
+
+//photo gallery -- collection of claudia's photos. when user clicks on next, another photo appears. repeat this process for 12 or so photos.
